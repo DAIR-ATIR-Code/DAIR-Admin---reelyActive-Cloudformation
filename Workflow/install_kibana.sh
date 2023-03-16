8f1cce67a1f7a92e7ed877ebdf023901df47fc1b
Segun Hassan <segun.hassan@canarie.ca>
2023-03-16 15:10:29 +0000

    Update 'Workflow/install_kibana.sh'

--- Workflow/install_kibana.sh
+++ Workflow/install_kibana.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # reelyActive-Install Kibana and enable as systemctl service
-
 sudo apt-get update && sudo apt-get install kibana
 sudo /bin/systemctl daemon-reload
 sudo /bin/systemctl enable kibana.service

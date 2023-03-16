305ef0725e58e6b8fbcb9dcf89a91b9f298b4b6e
Segun Hassan <segun.hassan@canarie.ca>
2023-03-16 18:22:41 +0000

    Update 'Workflow/Post-Provision/start_services.sh'

--- Workflow/Post-Provision/start_services.sh
+++ Workflow/Post-Provision/start_services.sh
@@ -2,4 +2,9 @@
 # reelyActive-Start Elasticsearch and Kibana
 
 echo "Starting Elasticsearch and Kibana"
-sudo systemctl start elasticsearch.service && sudo systemctl start kibana.service && echo "Done"
+sudo systemctl start elasticsearch.service
+sudo systemctl start kibana.service 
+sleep 10
+sudo systemctl status elasticsearch.service
+sudo systemctl status kibana.service 
+echo "Done"

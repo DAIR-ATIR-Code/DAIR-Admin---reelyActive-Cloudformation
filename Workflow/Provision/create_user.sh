101dace9edcd0ff366e24632ccbcdf017eab918f
Segun Hassan <segun.hassan@canarie.ca>
2023-03-16 16:48:39 +0000

    Update 'Workflow/Provision/create_user.sh'

--- Workflow/Provision/create_user.sh
+++ Workflow/Provision/create_user.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # reelyActive-Create reelyactive user
+
+echo "Creating reelyactive user"
 sudo adduser reelyactive --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
 echo "reelyactive:owl-in-one" | sudo chpasswd

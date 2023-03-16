04cbdefca9cccb37eee77f482cdee42ac8a42c73
Segun Hassan <segun.hassan@canarie.ca>
2023-03-16 16:50:22 +0000

    Update 'Workflow/Post-Provision/install_elasticsearch.sh'

--- Workflow/Post-Provision/install_elasticsearch.sh
+++ Workflow/Post-Provision/install_elasticsearch.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # reelyActive-Install Elasticsearch and enable as systemctl service
+
+echo "Installing Elasticsearch and enabling as systemctl service"
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 sudo apt-get install apt-transport-https
 echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list

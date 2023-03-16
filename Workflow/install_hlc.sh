8ade17457a3d6a9817cd01f5cf97bde1d3c433d9
Segun Hassan <segun.hassan@canarie.ca>
2023-03-16 15:13:14 +0000

    Add 'Workflow/install_hlc.sh'

---
+++ Workflow/install_hlc.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#reelyActive-Install hlc-server and run with systemctl
+sudo mkdir /opt/reelyActive
+sudo chown reelyactive /opt/reelyActive
+sudo chgrp reelyactive /opt/reelyActive
+cd /opt/reelyActive
+sudo -u reelyactive git clone https://github.com/reelyactive/hlc-server.git
+cd hlc-server
+sudo -H -u reelyactive npm install
+sudo cat > /lib/systemd/system/hlc.service << EOF
+[Unit]
+Description=reelyActive Hyperlocal Context Server
+After=network.target
+
+[Service]
+WorkingDirectory=/opt/reelyActive/hlc-server
+ExecStart=/usr/local/bin/npm start
+Restart=on-failure
+User=reelyactive
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo systemctl enable hlc.service
+sudo systemctl start hlc.service
